ac6ee92fbb31c3a8497ea79314b9d1ea929c0dd1
Radu Rares-Aurelian <59961516+raresradua@users.noreply.github.com>
2022-10-16 18:01:54 +0300

    Update ex3.py

--- Lab1/ex3.py
+++ Lab1/ex3.py
@@ -1,5 +1,7 @@
 # Write a script that receives two strings and prints the number of occurrences of the first string in the second.
-
+'''
+aici ar trebui sa tratezi si cazuri de genul first_string = 'aaa', second_string = 'aaaaa' -> 3 aparitii, nu 1
+'''
 if __name__ == '__main__':
     first_string = input()
     second_string = input()
