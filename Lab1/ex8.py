8e48bb273d5ebc52f1775566925eed698a0ee739
Radu Rares-Aurelian <59961516+raresradua@users.noreply.github.com>
2022-10-16 18:03:51 +0300

    Update ex8.py

--- Lab1/ex8.py
+++ Lab1/ex8.py
@@ -1,6 +1,8 @@
 # Write a function that counts how many bits with value 1 a number has. For example for number 24, the binary format
 # is 00011000, meaning 2 bits with value "1"
-
+'''
+e in regula dar ai putea pe viitor sa te folosesti de builtins, cum ar fi bin() si apoi sa faci count('1') pe rezultat, e in regula si asa
+'''
 if __name__ == '__main__':
     number = int(input())
     res = 0
