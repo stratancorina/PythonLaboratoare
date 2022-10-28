59cb2f1dc569fe5aeaaba66626434e1e8562e82d
Radu Rares-Aurelian <59961516+raresradua@users.noreply.github.com>
2022-10-28 10:33:48 +0300

    Update ex10.py

--- Lab2/ex10.py
+++ Lab2/ex10.py
@@ -1,7 +1,9 @@
 # Write a function that receives a variable number of lists and returns a list of tuples as follows: the first tuple
 # contains the first items in the lists, the second element contains the items on the position 2 in the lists,
 # etc. Ex: for lists [1,2,3], [5,6,7], ["a", "b", "c"] return: [(1, 5, "a ") ,(2, 6, "b"), (3,7, "c")].
-
+'''
+buna ideea cu zip, dar daca nu ai liste de aceeasi lungime nu prea merge, pierzi date, ar trb padate listele la aceeasi lungime
+'''
 def combinat(lists):
     z = list(zip(lists[0], lists[1], lists[2], lists[3]))
     return z
