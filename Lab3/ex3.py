5a669a1f3884d881fd13c9c4d2ef01b0b60cfbec
Radu Rares-Aurelian <59961516+raresradua@users.noreply.github.com>
2022-11-01 18:55:20 +0200

    Update ex3.py

--- Lab3/ex3.py
+++ Lab3/ex3.py
@@ -1,3 +1,10 @@
+'''
+aici lipseste recursivitatea si verificarea in functie de tipul valorilor si al valorilor din lista de valori a
+cheilor. ideea era ca poti avea dictionare nested la adancimi mai mari in dictionare, iar ideea era ca pe masura ce
+treci prin chei, daca toate cheile sunt aceleasi peste tot, treci la valorile din chei, daca valorile din chei au tip
+diferit / valori diferite atunci te opresti, iar daca dai peste dictionare ar trebui sa apelezi din nou aceeasi functie
+si tot asa
+'''
 def comparing(d1, d2):
     res = all((d1.get(k) == v for k, v in d2.items()))
     print(res)
